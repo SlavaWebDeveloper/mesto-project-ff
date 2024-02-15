f0230de90ab282b91954fd431cc048fb9ac747d9
Slava Sidorov <sidorov-vv3@mail.ru>
2024-02-15 18:11:17 +0700

    added function createCard

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+
+const placesList = document.querySelector('.places__list');
 
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
+function createCard(card, cardDelete) {
+  let cardElement = cardTemplate.querySelector('.card').cloneNode(true);  // шаблон для отдельной карточки
+  
+  // обновление данных
+  cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__title').textContent = card.name;
 
-// @todo: Вывести карточки на страницу
+  // добавление прослушки на кнопку 
+  cardElement.querySelector('.card__delete-button').addEventListener('click', cardDelete);
+  
+  return cardElement;
+}
+
+renderCard.forEach(function(card) {
+  placesList.append(createCard(card));
+})
+
+createCard(initialCards);
