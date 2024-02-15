b3b88f2ddde667d661ce4aa5932a4fdbeb2cc677
Slava Sidorov <sidorov-vv3@mail.ru>
2024-02-15 18:10:37 +0700

    new array

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
     {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      name: "Плюшевая акула",
+      link: "https://cdn.pixabay.com/photo/2024/01/27/10/23/shark-8535643_1280.jpg",
     },
     {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      name: "Бушующий океан в яблоке",
+      link: "https://cdn.pixabay.com/photo/2024/01/12/04/10/photoshop-8503075_1280.jpg",
     },
     {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      name: "Елочная игрушка 'Добби'",
+      link: "https://cdn.pixabay.com/photo/2023/12/20/10/27/christmas-8459516_1280.jpg",
     },
     {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      name: "Ваза с цветами",
+      link: "https://cdn.pixabay.com/photo/2023/10/02/11/56/flowers-8289075_1280.jpg",
     },
     {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      name: "Плюшевый осминог",
+      link: "https://cdn.pixabay.com/photo/2024/01/27/10/23/octopus-8535647_1280.jpg",
     },
     {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      name: "Пряничный дом",
+      link: "https://cdn.pixabay.com/photo/2023/10/02/11/51/gingerbread-8289064_1280.jpg",
     }
 ];
