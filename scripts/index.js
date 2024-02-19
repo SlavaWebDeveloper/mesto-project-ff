19120a42c8546c4aea3a935610c41516d6453de1
Slava Sidorov <sidorov-vv3@mail.ru>
2024-02-19 13:01:22 +0700

    added function cardDelete and review function createCard. Changed the method of calling the cardDelete function

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,29 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
 
+// @todo: DOM узлы
 const placesList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
-
 // @todo: Функция создания карточки
-
-function createCard(card, cardDelete) {
-  let cardElement = cardTemplate.querySelector('.card').cloneNode(true);  // шаблон для отдельной карточки
+function createCard(card) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);  // шаблон для отдельной карточки
   
   // обновление данных
   cardElement.querySelector('.card__image').src = card.link;
   cardElement.querySelector('.card__title').textContent = card.name;
-
-  // добавление прослушки на кнопку 
-  cardElement.querySelector('.card__delete-button').addEventListener('click', cardDelete);
   
+  cardElement.querySelector('.card__delete-button').addEventListener('click', function() {
+    cardDelete(cardElement);
+  });
+
   return cardElement;
 }
 
+function cardDelete(cardElement) {
+  cardElement.closest('.card').remove();
+}
+
+// @todo: Вывести карточки на страницу
 initialCards.forEach(function(card) {
   placesList.append(createCard(card));
 })
-
