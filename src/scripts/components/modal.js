2c13491bd5d6542427b888bf98805bb5b93606dd
Slava Sidorov <sidorov-vv3@mail.ru>
2024-03-29 13:57:46 +0700

    Added event listeners for opening modals and handling ESC key; updated index.js to use new openModal function with popup parameter

--- src/scripts/components/modal.js
+++ src/scripts/components/modal.js
@@ -1,7 +1,16 @@
-export function openModal() {
-
+const eventHandler = (e) => {
+  if (e.keyCode === 27) {
+    const popup = document.querySelector('.popup_is-opened');
+    closeModal(popup);
+  }
 }
 
-export function closeModal() {
-  
+export function openModal(popup) {
+  popup.classList.add('popup_is-opened');
+  document.addEventListener('keyup', eventHandler);
+}
+
+export function closeModal(popup) {
+  popup.classList.remove('popup_is-opened');
+  document.removeEventListener('keyup', eventHandler);
 }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,39 @@
-import './../styles/index.css'; 
-import {createCard, cardDelete} from  "./components/card";
-import {openModal, closeModal} from "./components/modal"
+import './../styles/index.css';
+import { createCard, cardDelete } from "./components/card";
+import { openModal, closeModal } from "./components/modal"
 import initialCards from './cards.js';
 
 const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
+
+const popupTypeEdit = document.querySelector('.popup_type_edit');
+const popupTypeNewCard = document.querySelector('.popup_type_new-card');
+const popupTypeImage = document.querySelector('.popup_type_image');
+const popupCloseAll = document.querySelectorAll('.popup');
+
+const content = document.querySelector('.content')
+
+content.addEventListener('click', function (event) {
+  event.target.classList.contains('profile__edit-button')? openModal(popupTypeEdit):'';
+
+  event.target.classList.contains('profile__add-button')? openModal(popupTypeNewCard):'';
   
+  event.target.classList.contains('card__image')? openModal(popupTypeImage):'';
+
+});
+
+popupCloseAll.forEach(popup => {
+  popup.addEventListener('click', function (event) {
+    if (event.target.closest('.popup__close') || event.target === popup) {
+      closeModal(popup);
+    }
+  });
+});
+
+
 /**
  * Выводит карточки из массива на веб-страницу
  */
-initialCards.forEach(function(card) {
+initialCards.forEach(function (card) {
   placesList.append(createCard(cardTemplate, card, cardDelete));
 })
